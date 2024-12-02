2457079ef1147a0b086e0ab6a4d0976c9bc24bbc
Adria Martinez <adrimm666@gmail.com>
2024-12-03 00:02:02 +0100

    App structure for user Authentication + navigation

--- App.tsx
+++ App.tsx
@@ -1,26 +1,63 @@
 // React
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+
+// Libraries
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Components
-import BottomTabNavigator from './src/components/navigation/BottomTabNav';
+import BottomTabNavigator from './src/components/navigation/BottomTabNavigator';
 
+// Screens
+import UserAuthScreen from './src/screens/UserAuthScreen';
 
-function App(): React.JSX.Element {
+const RootStack = createNativeStackNavigator();
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const token = await AsyncStorage.getItem('userToken');
+      if (token) {
+        setIsAuthenticated(true);
+      }
+    };
+    checkAuthentication();
+  }, []);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const storedToken = await AsyncStorage.getItem('userToken');
+      setToken(storedToken);
+    };
+    fetchToken();
+  }, [isAuthenticated]);
+
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <BottomTabNavigator />
+        <RootStack.Navigator>
+            {isAuthenticated ? (
+              <RootStack.Screen name="Home" component={BottomTabNavigator} />
+            ) : (
+              <RootStack.Screen name="UserAuth">
+                {props => <UserAuthScreen {...props} setIsAuthenticated={setIsAuthenticated} />}
+              </RootStack.Screen>
+            )}
+        </RootStack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',  // Fondo blanco, ajusta según el diseño
+    backgroundColor: '#fff',
   },
 });
 
