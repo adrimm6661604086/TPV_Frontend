6739f2a7df57947555a28c137b2dcefba162ecb6
Adria Martinez <adrimm666@gmail.com>
2024-12-03 17:47:27 +0100

    Theme for the app

---
+++ src/utils/theme.js
@@ -0,0 +1,34 @@
+const theme = {
+    palette: {
+        primary: {
+            main: '#4CAF50',
+            light: '#80E27E',
+            dark: '#087F23',
+            white: '#FFFFFF',
+        },
+        secondary: {
+            main: '#94C595',
+        },
+        background: {
+            default: '#101010',
+            light: '#F5F5F5',
+        },
+        text: {
+            primary: '#000',
+            secondary: '#888888',
+            light: '#CCCCCC',
+            color: '#196f3d'
+        },
+        success: {
+            main: '#2C6E49',
+        },
+        info: {
+            main: '#93C7B5',
+        },
+        error: {
+            main: '#B00020',
+        },
+    },
+};
+
+export default theme;
