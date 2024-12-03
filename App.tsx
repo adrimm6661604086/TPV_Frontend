71dc33eefb5a2f521466e9e319d83ae14d8aac02
Adria Martinez <adrimm666@gmail.com>
2024-12-03 17:44:31 +0100

    Root Navigator added to App

--- App.tsx
+++ App.tsx
@@ -7,16 +7,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Components
+// Navigators
 import BottomTabNavigator from './src/components/navigation/BottomTabNavigator';
+import PaymentNavigator from './src/components/navigation/PaymentNavigtor';
+
 
 // Screens
-import UserAuthScreen from './src/screens/UserAuthScreen';
+import UserAuthScreen from './src/screens/User/UserAuthScreen';
+import ProfileScreen from './src/screens/User/ProfileScreen';
+import { RootStackParamList } from './src/types/navigationTypes';
 
-const RootStack = createNativeStackNavigator();
+// Utils
+import theme from './src/utils/theme';
 
-const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
   const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
@@ -40,14 +48,38 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
+        {/* {isAuthenticated ? (
+          <BottomTabNavigator />
+        ) : (
+          <UserAuthScreen setIsAuthenticated={setIsAuthenticated} />
+        )} */}
         <RootStack.Navigator>
             {isAuthenticated ? (
-              <RootStack.Screen name="Home" component={BottomTabNavigator} />
+              <RootStack.Screen 
+                name="Main" 
+                component={BottomTabNavigator} 
+                options={{ headerShown: false }}
+              >
+              </RootStack.Screen>
             ) : (
-              <RootStack.Screen name="UserAuth">
+              <RootStack.Screen 
+                name="UserAuth"
+                options={{ headerShown: false }}
+              >
                 {props => <UserAuthScreen {...props} setIsAuthenticated={setIsAuthenticated} />}
               </RootStack.Screen>
             )}
+            <RootStack.Screen 
+              name="Profile"
+              component={ProfileScreen}
+              options={{ headerShown: false }}
+            />
+            <RootStack.Screen
+              name="Payment"
+              component={PaymentNavigator}
+              options={{ headerShown: false }}
+            />
+
         </RootStack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
@@ -57,7 +89,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.palette.background.light,
   },
 });
 
