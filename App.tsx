7780982dc250cdf543376c2911ea34e5b4c035e4
Adria Martinez <adrimm666@gmail.com>
2025-01-11 01:01:21 +0100

    Delete unused lines

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 // React
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 // Libraries
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,6 +13,7 @@ import PaymentNavigator from './src/components/navigation/PaymentNavigtor';
 // Screens
 import UserAuthScreen from './src/screens/User/UserAuthScreen';
 import ProfileScreen from './src/screens/User/ProfileScreen';
+import MoreInfoScreen from './src/screens/Other/MoreInfoScreen';
 import { RootStackParamList } from './src/types/navigationTypes';
 
 // Hooks
@@ -20,13 +21,34 @@ import { useVerifyAuth } from './src/hooks/UserHooks';
 
 // Utils
 import theme from './src/utils/theme';
+import { BACKEND_URL } from '@env';
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-
+  // const [message, setMessage] = useState<string>('');
+  
   useVerifyAuth(setIsAuthenticated);
+
+  // useEffect(() => {
+  //   const testFetch = async () => {
+  //     try {
+  //     const response = await fetch(`${BACKEND_URL}/api/test`, {
+  //       method: 'GET',
+  //       headers: {
+  //       'Content-Type': 'application/json',
+  //       },
+  //     });
+  //     const data = await response.json();
+  //     setMessage(data.message);
+  //     } catch (error) {
+  //     console.error('Error testing:', error);
+  //     }
+  //   }
+  //   testFetch();
+  // }, []);
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -58,10 +80,23 @@ const App: React.FC = () => {
               component={PaymentNavigator}
               options={{ headerShown: false }}
             />
+            <RootStack.Screen
+              name="ShowMore"
+              options={{ headerShown: false }}
+            >
+              {props => {
+                const { route } = props;
+                const transactions = route.params?.transactions;
+                return <MoreInfoScreen {...props} transactions={transactions} />
+              }}
+            </RootStack.Screen>
 
         </RootStack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
+    // <SafeAreaView>
+    //   <Text>{message}</Text>
+    // </SafeAreaView>
   );
 };
 
