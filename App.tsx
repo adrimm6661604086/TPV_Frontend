092aa0f4e8ec341bdb9c6c37376ef93cef9140f2
Adria Martinez <adrimm666@gmail.com>
2025-02-16 01:07:43 +0100

    Libraries added

--- App.tsx
+++ App.tsx
@@ -22,6 +22,7 @@ import { useVerifyAuth } from './src/hooks/UserHooks';
 // Utils
 import theme from './src/utils/theme';
 import { BACKEND_URL } from '@env';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
@@ -51,51 +52,55 @@ const App: React.FC = () => {
 
 
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <RootStack.Navigator>
-            {isAuthenticated ? (
+    <GestureHandlerRootView>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <RootStack.Navigator>
+              {isAuthenticated ? (
+                <RootStack.Screen 
+                  name="Main" 
+                  component={BottomTabNavigator} 
+                  options={{ headerShown: false }}
+                >
+                </RootStack.Screen>
+              ) : (
+                <RootStack.Screen 
+                  name="UserAuth"
+                  options={{ headerShown: false }}
+                >
+                  {props => <UserAuthScreen {...props} setIsAuthenticated={setIsAuthenticated} />}
+                </RootStack.Screen>
+              )}
               <RootStack.Screen 
-                name="Main" 
-                component={BottomTabNavigator} 
+                name="Profile"
                 options={{ headerShown: false }}
               >
+                {props => <ProfileScreen {...props} setIsAuthenticated={setIsAuthenticated} />}
               </RootStack.Screen>
-            ) : (
-              <RootStack.Screen 
-                name="UserAuth"
+              <RootStack.Screen
+                name="Payment"
+                component={PaymentNavigator}
+                options={{ headerShown: false }}
+              />
+              <RootStack.Screen
+                name="ShowMore"
                 options={{ headerShown: false }}
               >
-                {props => <UserAuthScreen {...props} setIsAuthenticated={setIsAuthenticated} />}
+                {props => {
+                  const { route } = props;
+                  const transactions = route.params?.transactions;
+                  return <MoreInfoScreen {...props} transactions={transactions} />
+                }}
               </RootStack.Screen>
-            )}
-            <RootStack.Screen 
-              name="Profile"
-              options={{ headerShown: false }}
-            >
-              {props => <ProfileScreen {...props} setIsAuthenticated={setIsAuthenticated} />}
-            </RootStack.Screen>
-            <RootStack.Screen
-              name="Payment"
-              component={PaymentNavigator}
-              options={{ headerShown: false }}
-            />
-            <RootStack.Screen
-              name="ShowMore"
-              options={{ headerShown: false }}
-            >
-              {props => {
-                const { route } = props;
-                const transactions = route.params?.transactions;
-                return <MoreInfoScreen {...props} transactions={transactions} />
-              }}
-            </RootStack.Screen>
 
-        </RootStack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
-    // <SafeAreaView>
-    //   <Text>{message}</Text>
+          </RootStack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+
+
+    // <SafeAreaView style={styles.container}>
+    //   <Text>{message ? message : 'Error in request'}</Text>
     // </SafeAreaView>
   );
 };
